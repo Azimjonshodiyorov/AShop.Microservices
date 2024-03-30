f648364ba2dbde6a0b11e4c07942bd1acae20214
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-03-30 17:41:23 +0500

    Basket Modul Finished

--- src/WebAPI/AShop.Basket.WebAPI/Program.cs
+++ src/WebAPI/AShop.Basket.WebAPI/Program.cs
@@ -2,10 +2,17 @@ using System.Reflection;
 using AShop.Basket.Application.GrpcService;
 using AShop.Basket.Application.Handlers;
 using AShop.Basket.Infrastructure.Repositories;
+using AShop.Basket.WebAPI.Swagger;
 using AShop.Common.Logging.Correlation;
 using Discount.Grpc.Protos;
+using HealthChecks.UI.Client;
+using MassTransit;
+using MassTransit.MultiBus;
 using MediatR;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +31,7 @@ builder.Services.AddVersionedApiExplorer(option =>
 {
     option.GroupNameFormat = "'v'VVV";
     option.SubstituteApiVersionInUrl = true;
+});
     builder.Services.AddApiVersioning();
     builder.Services.AddCors(option =>
     {
@@ -32,8 +40,8 @@ builder.Services.AddVersionedApiExplorer(option =>
             policy.AllowAnyHeader().AllowAnyMethod().AllowAnyMethod();
         });
     });
-});
 
+builder.Services.AddHealthChecks();
 builder.Services.AddStackExchangeRedisCache(option =>
 {
     option.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
@@ -47,18 +55,40 @@ builder.Services.AddScoped<DiscountGrpcService>();
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(x =>
     x.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUr"]));
 
+builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
+builder.Services.AddSwaggerGen();
 
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();  
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+
+        /*foreach (var description in .ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+        }*/
+    });
 }
+        
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("CorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("/health", new HealthCheckOptions
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+});
 
 app.Run();
-
