445788ea242a1b05f97a88ff24fae215210f460e
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-01 00:33:04 +0500

    Create Behaviour

---
+++ src/Application/AShop.Order.Application/Behaviour/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace AShop.Order.Application.Behaviour;
+
+public class UnhandledExceptionBehaviour<TRequst ,TResponse> : IPipelineBehavior<TRequst , TResponse> where TRequst : IRequest<TResponse>
+{
+    private readonly ILogger<TRequst> _logger;
+
+    public UnhandledExceptionBehaviour(ILogger<TRequst> logger)
+    {
+        _logger = logger;
+    }
+    public async Task<TResponse> Handle(TRequst request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception e)
+        {
+            var requstName = typeof(TRequst).Name;
+            _logger.LogError(e,$"Unhandled Exception Requst name : {requstName} , {request}");
+            throw;
+        }
+    }
+}
---
+++ src/Application/AShop.Order.Application/Behaviour/ValidationBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace AShop.Order.Application.Behaviour;
+
+public class ValidationBehaviour<TRequest , TResponse> : IPipelineBehavior<TRequest ,TResponse>  where TRequest : IRequest<TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResult =
+                await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));
+            var failures = validationResult.SelectMany(e => e.Errors).Where(f => f != null).ToList();
+            if (failures.Count != 0)
+            {
+                throw new ValidationException(failures);
+            }
+        }
+
+        return await next();
+    }
+}
