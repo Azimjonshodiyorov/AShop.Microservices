5282490e2f4f5a737aa7a91d42eb7dc442e64e53
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-01 00:48:34 +0500

    Excaption modul create

---
+++ src/Application/AShop.Order.Application/Excaptions/OrderNotFoundExcaption.cs
@@ -0,0 +1,10 @@
+﻿namespace AShop.Order.Application.Excaptions;
+
+public class OrderNotFoundExcaption : ApplicationException
+{
+    public OrderNotFoundExcaption(string name, object key) 
+        : base($"Entity {name} - {key} is not found.")
+    {
+        
+    }
+}
---
+++ src/Application/AShop.Order.Application/Excaptions/ValidationExcaption.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+namespace AShop.Order.Application.Excaptions;
+
+public class ValidationExcaption : ApplicationException
+{
+   public IDictionary<string , string[]> Error { get; }
+
+   public ValidationExcaption() : base("one or more validation error(s) .")
+   {
+      Error = new Dictionary<string, string[]>();
+   }
+
+   public ValidationExcaption(IEnumerable<ValidationFailure> failures) : this()
+   {
+      Error = failures.GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+         .ToDictionary(x => x.Key, x => x.ToArray());
+   }
+}
