d478f7bb571ce483b276bf5077fea97e86eb33a9
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-02 15:50:47 +0500

    bug

--- src/WebAPI/AShop.Order.WebAPI/Program.cs
+++ src/WebAPI/AShop.Order.WebAPI/Program.cs
@@ -1,8 +1,17 @@
+using AShop.Common.Logging;
+using AShop.EventBus.Message.Common;
 using AShop.EventBus.Message.Events;
 using AShop.Order.Application.Extensions;
 using AShop.Order.Infrastructure.Extension;
+using AShop.Order.Infrastructure.OrderDbContext;
+using AShop.Order.WebAPI.EventBusConsumer;
+using AShop.Order.WebAPI.Extensions;
 using HealthChecks.UI.Client;
+using MassTransit;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +22,45 @@ builder.Services.AddApiVersioning();
 builder.Services.AddApplicationService();
 builder.Services.AddInfraServices(builder.Configuration);
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddScoped<BasketCheckoutEvent>();
-builder.Services.AddScoped<BasketCheckoutEventV2>();
+builder.Services.AddScoped<BasketOrderConsumer>();
+builder.Services.AddScoped<BasketOrderConsumerV2>();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1" , new OpenApiInfo{Title = "Ordering.Api" , Version = "v1"});
+});
+
+builder.Services.AddVersionedApiExplorer(option =>
+{
+    option.GroupNameFormat = "'v'VVV";
+    option.SubstituteApiVersionInUrl = true;
+});
+builder.Services.AddHealthChecks().Services.AddDbContext<OrderContext>();
+
+builder.Services.AddMassTransit(config =>
+{
+    config.AddConsumer<BasketOrderConsumer>();
+    config.AddConsumer<BasketOrderConsumerV2>();
+    
+    config.UsingRabbitMq((context, confi) =>
+    {
+        confi.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+        confi.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue , x =>
+        {
+            x.ConfigureConsumer<BasketOrderConsumer>(context);
+        });
+        
+        confi.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueueV2 , x =>
+        {
+            x.ConfigureConsumer<BasketOrderConsumerV2>(context);
+        });
+    });
+});
+builder.Services.AddMassTransitHostedService();
+builder.Host.UseSerilog(Logging.ConfigureLogger);
 
 var app = builder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -36,6 +78,12 @@ app.UseEndpoints(endpoints =>
         Predicate = _ => true,
         ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
     });
+});
+
+app.MigrateDatabase<OrderContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<OrderContexSeed>>();
+    OrderContexSeed.SeedAsync(context, logger).Wait();
 });
 
 app.Run();
