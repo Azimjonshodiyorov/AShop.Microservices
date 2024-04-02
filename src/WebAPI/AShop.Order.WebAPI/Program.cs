10b87b48f551dd7149b3ce00c2a865eabdfa8576
Azimjon Shodiyorov <azimjonshodiyorov@gmail.com>
2024-04-02 15:10:14 +0500

    configuration

--- src/WebAPI/AShop.Order.WebAPI/Program.cs
+++ src/WebAPI/AShop.Order.WebAPI/Program.cs
@@ -1,17 +1,41 @@
+using AShop.EventBus.Message.Events;
+using AShop.Order.Application.Extensions;
+using AShop.Order.Infrastructure.Extension;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
+builder.Services.AddApplicationService();
+builder.Services.AddInfraServices(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<BasketCheckoutEvent>();
+builder.Services.AddScoped<BasketCheckoutEventV2>();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1"));
 }
 
-app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHealthChecks("/health", new HealthCheckOptions
+    {
+        Predicate = _ => true,
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+});
 
 app.Run();
